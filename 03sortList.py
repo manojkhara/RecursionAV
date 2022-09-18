567e8841d2d2a50acce126779063673a99d9870b
manojkhara <58291240+manojkhara@users.noreply.github.com>
2022-09-18 19:56:51 +0530

    Add files via upload

---
+++ 03sortList.py
@@ -0,0 +1,26 @@
+#Recursion
+#Sort an list using Recursion
+# youtube Link given below
+#https://www.youtube.com/watch?v=AZ4jEY_JAVc&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=6&ab_channel=AdityaVerma
+
+def sorting(lst):
+    # base case
+    if(len(lst)<=1):
+        return
+    #code
+    temp=lst.pop()   #store last element of list
+    sorting(lst)     #keep removing last element of list until only one element remains
+    insertion(lst,temp) #insert function
+
+
+def insertion(lst,temp):
+    if(len(lst)==0 or lst[len(lst)-1]<temp):      # if no element remains in list then append or temp is greater than last element
+        lst.append(temp)
+        return
+    x = lst.pop()    #insertion at proper position will only happens if there are no elements present in the list
+    insertion(lst,temp)
+    lst.append(x)     #inserting after all the above commands destacks
+
+lst=[5,3,2,4,-5,1,6,-1,9,0]
+sorting(lst)
+print(lst)
