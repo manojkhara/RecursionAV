6dc0b5f807827697fe79a7b9f2a3e95498ff7cf6
manojkhara <manojkhara131@gmail.com>
2022-10-03 15:56:30 +0530

    sort a stack updated

--- 04sortStack.py
+++ 04sortStack.py
@@ -1,21 +1,21 @@
 # stack Last in first out
 
-def sortStack(arr):
-    if len(arr) <= 1 :    # if there is only one element in stack than its sorted
+def sortStack(stack):
+    if len(stack) <= 1 :    # if there is only one element in stack than its sorted
         return
-    num = arr.pop()      # if not 
-    sortStack(arr)
+    num = stack.pop()      # if not 
+    sortStack(stack)
 
     # insert function will put num at right position in sorted stack
-    def insert(arr,num):
-        if arr == [] or arr[len(arr)-1] < num: 
-            arr.append(num)
+    def insert(stack,num):
+        if stack == [] or stack[len(stack)-1] < num: 
+            stack.append(num)
             return
-        temp = arr.pop()   # stored the top element because it was bigger than num
-        insert(arr,num)    # again checking if num can be appended at top
-        arr.append(temp)   # after sorting remaining element in stack, add the temp 
+        temp = stack.pop()   # stored the top element because it was bigger than num
+        insert(stack,num)    # again checking if num can be appended at top
+        stack.append(temp)   # after sorting remaining element in stack, add the temp 
     
-    insert(arr,num)
+    insert(stack,num)
 
 
 stack =[5,3,2,4,-5,1,6,-1,9,0]
