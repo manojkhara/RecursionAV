5ea239f87ff3c2695ffa5564a0c7a3f9178ce570
manojkhara <manojkhara131@gmail.com>
2022-10-18 18:17:32 +0530

    Tower of Hanoi

--- 08towerOfHanoi.py
+++ 08towerOfHanoi.py
@@ -1,25 +1,41 @@
 # https://en.wikipedia.org/wiki/Tower_of_Hanoi
 
+"""
+For n = 3 plates on Source
 
-def towerOfHanoi(s:str, d:str, h:str, n:int):
-    if n==1:
-        print(f"moving plate {n} from {s} to {d}")
+step 1: moving plate 1 from Source to Destination
+step 2: moving plate 2 from Source to Helper
+step 3: moving plate 1 from Destination to Helper
+step 4: moving plate 3 from Source to Destination
+step 5: moving plate 1 from Helper to Source
+step 6: moving plate 2 from Helper to Destination
+step 7: moving plate 1 from Source to Destination
+
+"""
+
+def towerOfHanoi(n:int,  source :str, helper:str, destination:str):
+
+    global counter
+    counter += 1
+
+    # Base Case    
+    # when only last one plate remains on the source rod
+    # transfer it to destination
+ 
+    if n == 1:
+        print(f"moving plate {n} from {source} to {destination} ")
         return
 
-    towerOfHanoi(s, h, d, n-1)
+    towerOfHanoi(n-1, source, destination, helper)     #sending n-1 plates from source to helper rod
 
-    print(f"moving plate {n} from {s} to {d}")
-    towerOfHanoi(h,d,s,n-1)
+    print(f"moving plate {n} from {source} to {destination}")
+    
+    towerOfHanoi(n-1, helper, source, destination)     #sending n-1 plates from helper to destination
 
     return
 
-
-
 #driver code
-
-s = "Source"
-h = "Helper"
-d = "Destination"
-n = 4
-
-towerOfHanoi(s, d, h, n)
+n = int(input("Enter number of plates:  "))     #don't enter high number stack will overflow
+counter = 0
+towerOfHanoi(n, "Source", "Helper", "Destination")
+print(f"\ntotal {counter} steps required to transfer {n} plates")     # steps = (2 ** n) - 1 (but here counting using counter)
